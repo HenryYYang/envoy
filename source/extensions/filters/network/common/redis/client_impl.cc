88baa9b66e9522233e84eee6724041f3d0115c76
Nicolas Flacco <nflacco@lyft.com>
2020-02-13 10:50:56 -0800

    format
    Signed-off-by: Nicolas Flacco <nflacco@lyft.com>

--- source/extensions/filters/network/common/redis/client_impl.cc
+++ source/extensions/filters/network/common/redis/client_impl.cc
@@ -113,9 +113,9 @@ PoolRequest* ClientImpl::makeRequest(const RespValue& request, ClientCallbacks& 
 
     // Try getting singleton
     // Two options here:
-    // 1) Somehow make the pointer castable (ie don't use void, store a subclass)
+    // 1) Somehow make the pointer cast-able (ie don't use void, store a subclass)
     // 2) Somehow cast a void pointer. Could we use generics under the hood here even due to
-    // virtualness!?
+    // virtualize!?
     auto x = host_->cluster().getUpstreamSpecificData(RedisCommandStatsFactory::UPSTREAM_MAP_KEY);
     x.get(); // NO-OP
     // const RedisCommandStatsSharedPtr command_stats_factory = reinterpret_cast<const
